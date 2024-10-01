cf4a39d955eb36fe1aa473a5f92893b76115837d
Sara Gonçalves De Lima <saragdl7321@gmail.com>
2024-10-01 20:39:19 -0300

    FIX[manter incomes] - Ajustando regras de negócio dos ganhos

--- src/main/java/br/ueg/progweb2/arquitetura/exceptions/ErrorValidation.java
+++ src/main/java/br/ueg/progweb2/arquitetura/exceptions/ErrorValidation.java
@@ -5,11 +5,13 @@ import lombok.Getter;
 @Getter
 public enum ErrorValidation {
 
+
     GENERAL("ER500", "Undefined error!"),
     INVALID_ID("ER400", "Invalid Id!"),
     NOT_FOUND("ER404", "Not found!"),
     MANDATORY_FIELD_VIOLATION("ER403", "Mandatory field must be filled up!"),
-    USER_ALREADY_EXISTS("ER405","User already exists");
+    USER_ALREADY_EXISTS("ER405","User already exists"),
+    BUSINESS_LOGIC_VIOLATION("ER407","Business logic violation!");
 
     private String code;
     private String message;
